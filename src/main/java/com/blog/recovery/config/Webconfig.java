b45e2735b3efe6e452bc2098cea4b078b0fcae95
kwon93 <93umsongaji@gmail.com>
2023-12-06 22:18:54 +0900

    study: Vue.js frontEnd 작업 진행

---
+++ src/main/java/com/blog/recovery/config/Webconfig.java
@@ -0,0 +1,18 @@
+package com.blog.recovery.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//@Configuration
+public class Webconfig implements WebMvcConfigurer {
+
+    /*
+     * CORS 에러를 서버측에서 해결하는 방법.
+     */
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173");
+//    }
+}
